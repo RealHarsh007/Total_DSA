f078114baa9fc0ea96616d14c9dc96d7ec33a719
Harshit Gupta <hg0910893@gmail.com>
2026-08-08 23:04:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.26%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+     
+     int revn=0;
+     while(x!=0)
+     {
+        int dig=x%10;
+        if(revn>INT_MAX/10||revn<INT_MIN/10)return 0;
+        revn=(revn*10)+dig;
+        x/=10;
+     }
+
+return revn;
+    }
+};
